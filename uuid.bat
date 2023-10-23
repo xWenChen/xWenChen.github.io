fe2968a83cd40ddbd2becf05480bc83594cd3601
xWenChen <986814308@qq.com>
2023-10-23 20:42:47 +0800

    添加生成 uuid 的脚本

--- uuid.bat
+++ uuid.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
+rem "生成 uuid 的脚本"
+
 for /f "tokens=* USEBACKQ" %%F in (`powershell -command "[guid]::NewGuid().ToString().ToUpper()"`) do (
   set "guid=%%F"
 )
