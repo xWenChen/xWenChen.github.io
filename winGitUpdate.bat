cb15fd4c197f7288774c805cd1c87f18036d2e44
xWenChen <986814308@qq.com>
2023-10-26 16:06:49 +0800

    调试windows下的git执行脚本

--- winGitUpdate.bat
+++ winGitUpdate.bat
@@ -3,12 +3,12 @@
 REM windows git 刷新脚本
 
 set msg=%1
-set need_push=%2
+set not_push=%2
 
 REM 检查分支名是否为空，为空则提示用户，并提前返回
 if "%msg%" == "" (
     echo.
-    echo -e 提交信息为空，请输入提交信息。提示：脚本运行命令如： gitUpdate.sh 更新脚本信息
+    echo -e 提交信息为空，请输入提交信息。提示：脚本运行命令如：'winGitUpdate.bat 更新脚本信息' 或者 'winGitUpdate.bat 更新脚本信息 true'
     echo.
 
     exit /b 1 REM 0 表示成功，非 0 表示失败
@@ -24,7 +24,7 @@ echo "执行 git commit -m %msg%"
 echo.
 git commit -m "%msg%"
 
-if %need_push% (
+if "%not_push%" != "true" (
     echo.
     echo "执行 git push"
     echo.
